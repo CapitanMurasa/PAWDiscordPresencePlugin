46b99438ea7bf52644930c34d9fa1cb1d9b488eb
Vlad Kolodiy <vladislavkolodiy45@gmail.com>
2026-02-09 21:00:40 +0200

    added gitignore

---
+++ discord.py
@@ -0,0 +1,11 @@
+import PAW_python as paw
+
+paw.PlayFile("C:\\Users\\MURASA\\Documents\\05 暁Records - La Sirène.flac")
+print(paw.GetFile())
+print(paw.GetTitle())
+print(paw.GetArtist())
+print(paw.GetAlbum())
+print(paw.GetGenre())
+print(paw.GetTimeElapsed())
+print(paw.GetTotalTime())
+        
