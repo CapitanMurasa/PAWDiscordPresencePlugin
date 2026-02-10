89e22c252a6f5aec3840d5101ddf26843d94130b
Vlad Kolodiy <vladislavkolodiy45@gmail.com>
2026-02-10 21:43:57 +0200

    UPdated plugin

--- discord.py
+++ discord.py
@@ -1,11 +1,35 @@
 import PAW_python as paw
+from pypresence import Presence
+import time
 
-paw.PlayFile("C:\\Users\\MURASA\\Documents\\05 暁Records - La Sirène.flac")
-print(paw.GetFile())
-print(paw.GetTitle())
-print(paw.GetArtist())
-print(paw.GetAlbum())
-print(paw.GetGenre())
-print(paw.GetTimeElapsed())
-print(paw.GetTotalTime())
-        
+player = paw.PAW()
+player.setInfo("Discord PAW plugin")
+
+try:
+    drp = Presence("1470437029253873861")
+    drp.connect()
+    print("Discord Connected")
+except Exception as e:
+    print(f"Discord failed: {e}")
+
+def update():
+    try:
+        if player.IsPlaybackActive():
+            drp.update(
+                details = "Playing: " + player.GetTitle(),
+                state = "By: " + player.GetArtist(),
+                large_image = "play",
+                large_text = "Playing on PerfectAudioWorks",
+                small_image = "icon_256",
+                small_text = "PerfectAudioWorks"
+            )
+        else:
+            drp.clear()
+            
+    except Exception as e:
+        print(f"RPC Error: {e}")
+
+
+player.register_update(update, 5000)
+
+print("Discord Plugin Registered Successfully")
